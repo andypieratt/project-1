72a7045d53b7a5d290634b571d91f13fc069dc2c
Alexb <acbird.ab@gmail.com>
2022-05-17 13:07:40 -0600

    spotify searchable player

--- Assets/Js/Script.js
+++ Assets/Js/Script.js
@@ -1,13 +1,11 @@
-//get text from inpput field and save to local storage
-
 $("#searchBtn").click(function () {
   var searchEl = $("#searchInput").val();
   localStorage.setItem("artist-name", searchEl);
   getArtist();
-  getUri();
-  //addId();
+  addId();
 });
 
+//
 function getArtist() {
   var artistName = localStorage.getItem("artist-name");
   var options = {
@@ -17,36 +15,27 @@ function getArtist() {
       "X-RapidAPI-Key": "d5b7852d0amshae97f9f551ea2a3p1e6b2djsn39ffa901b56b",
     },
   };
-
   fetch(
     "https://spotify23.p.rapidapi.com/search/?q=" +
       artistName +
       "&type=multi&offset=0&limit=10&numberOfTopResults=5",
     options
   )
-    .then((response) => response.json())
-    .then((response) => console.log(response))
-    .catch((err) => console.error(err));
-
-  //var artistUri = response.artists.items; //[0].data.uri;
-  //console.log(artistUri);
-  //localStorage.setItem("artistUri", artistUri);
-}
-function getUri() {
-  var artistUri = response.artists.items[0].data.uri;
-  console.log(artistUri);
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (data) {
+      var artistUri = data.artists.items[0].data.uri;
+      console.log(artistUri);
+      var uri = artistUri.substring(15);
+      localStorage.setItem("uri", uri);
+    });
 }
 
-// var uri = artistUri.substring(16, 37);
-//var spotifyLink = localStorage.getItem("artist-id");
-
-// function addId {
-//   var searchEl = $("#searchInput").val();
-//   localStorage.setItem("artist-id", searchEl);
-//   $("#spotifyWidget").attr(
-//     "src",
-//     "https://open.spotify.com/embed/playlist/" +
-//       uri +
-//       "?utm_source=generator"
-//   );
-// });
+function addId() {
+  var uri = localStorage.getItem("uri");
+  $("#spotifyWidget").attr(
+    "src",
+    "https://open.spotify.com/embed/artist/" + uri + "?utm_source=generator"
+  );
+}
